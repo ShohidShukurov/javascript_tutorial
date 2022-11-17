0ce06a5317ebccb9e37867621b4b3e5e9cf4178b
Shohid <shukurovshohid7@gmail.com>
2022-11-17 18:14:16 +0500

    Switch statement, For Loops

---
+++ 11-16-tutorial/lesson.js
@@ -0,0 +1,14 @@
+// let data = "qor";
+// switch (data) {
+//   case "yomg`ir":
+//     console.log("havo yomg`irli");
+//   case "qor":
+//     console.log("qor yog`moqda");
+//     break;
+//   case "qor":
+//     console.log("qor yog`moqda");
+
+// for (i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) continue; console.log(` ${i}`);
+
+// }
