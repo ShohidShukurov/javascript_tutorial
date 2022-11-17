2558331110a34431005e99968ab25783254ddcc6
Shohid <shukurovshohid7@gmail.com>
2022-11-17 17:51:34 +0500

    Type Conversion, Comparision, Logical Operators

---
+++ 11-9-tutorial/index.js
@@ -0,0 +1,58 @@
+//let a = 1, b=1
+
+//let c = ++a; javobi ikki chiqadi chunki a bizda 1ga teng oldidagi ikkinchi + 1ni yana 1taga oshiradi
+
+//let d = b++ // javobi 1 chiqadi 
+
+//console.log(c);
+
+//=================
+
+//let a = 2;
+//let x = 1 + (a *=2); javob 5 chiqadi chunki (a = a * 2) ga teng qavs ichidan 4 chiqadi va 1 qushilsa 5 buladi
+
+//==================
+
+// "" + 1 +0 javob 10 (string) chunki boshida string kelgani uchun qolganlariham stringga utadi 
+
+// "" - 1 + 0 javob -1 (number) chunki boshida string kelsa ham - bulgani uchun numberda qoledi
+
+
+// true + false javob 1 chunki true 1ga teng , false 0 ga
+
+// 6 / "3" javob 2 chunki (/) stringni num ga utkazib yubaryapti
+
+// 4 + 5 + "px" javob 9px (string) chiqadi px di oldida + kelgani uchun oldindagilarni ham stringga o`zgartidai
+
+// "$" + 4 + 5 javob "$"45 (string) chunki hammasi stringga o`tib qoladi
+
+// "4" - 2 javob 2 (number) chunki - stringdi numbrga uzgartiradi 
+
+ // "4px" - 2 javob NaN chunki px son emas
+
+ // " -9 " + 5 javob -9 5 (string) chiqadi
+
+// " -9 " - 5 javob -14 (number) chiqadi
+
+// javob 1 (number) chiqadi chunki null 0 ga teng
+
+//undefined + 1 NaN chiqadi chunki undefinet yuq narsa 
+
+// "\t \n" - 2 javobi -2 chiqadi
+
+//comparision
+
+// 5 > 4 javob (true) chunki 5 4dan katta
+
+// "apple" > "pineapple" (false) chunki apple dagi harflar kam va bu yerda asce table ga qaraladi
+
+// "2" > "12" javob (true) chunki 
+
+//undefined == null javob (true)
+
+//undefined == null javob (false) chunki qatiy tenglik ularning data type lariniham tekshiradi bular data type lari br xil emas
+
+// null == "\n0\n" javob (false)
+
+// null === "\n0\n" javob (false)
+ 
