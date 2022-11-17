22cbae7fa784356c7e3f20e90d68e61500f87a59
Shohid <shukurovshohid7@gmail.com>
2022-11-17 17:58:29 +0500

    If else, Ternarny operator, Nullish

---
+++ 11-15 tutorial/index.js
@@ -0,0 +1,27 @@
+// if else, teranry operation, nullish operation.
+
+// temp =30
+
+// if (temp  < 0){
+//     console.log("havo judayam sovuq");
+// // } else if (temp < 10 && temp >= 6) {
+// //     console.log("havo sovuq");
+// }else if (temp < 15 && temp >= 10 ) {
+//     console.log("havo issiq");
+// }else if (temp < 25) {
+//     console.log("judayam issiq");
+// } else {
+//     console.log("bunday malumot yuq")
+// }
+
+//teranry operation
+// ?=if :=else
+
+// temp = -1 ? console.log("havo judayam sovuq") : console.log("havo sovuq");
+// temp = 12 ? console.log("havo issiq") : console.log("havo judayam issiq");
+
+// = null undefined.
+
+// || = 0, false, null, undefined
+// console.log(0 || false || 0); true
+// console.log(false ?? true); false
