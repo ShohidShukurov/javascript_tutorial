471c1696231e515e1937ed2878f3259515575acc
Shohid <shukurovshohid7@gmail.com>
2022-11-20 19:22:58 +0500

    11-18-tutorial

---
+++ 11-18-tutorial/index.js
@@ -0,0 +1,67 @@
+// for (let i = 1; i < 10; i++) {
+//   console.log(i);
+
+// let i = 1;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+
+// console.log(a);
+// let a = 10;
+
+// function newfunction() {
+//   console.log("Muhammad");
+//   console.log("Muhammad");
+//   console.log("Muhammad");
+//   console.log("Muhammad");
+//   console.log("Muhammad");
+//   console.log("Muhammad");
+//   console.log("Muhammad");
+//   console.log("Muhammad");
+//   console.log("Muhammad");
+//   console.log("Muhammad");
+// }
+// newfunction();
+
+// const newfunction = function () {
+//   console.log("Muhammad");
+// };
+
+// newfunction()
+
+//arrow function
+
+// const newfunction = () => {
+//     console.log("MUHAMMAD");
+// };
+// newfunction()
+
+// const studentList = function (name, surname, age,) {
+//   console.log(name, surname, age);
+// };
+
+// studentList("Muhammad", "Umarov","23", );
+// studentList("Umar","Umarov", "20", );
+// studentList("Ali", "Umarov","21", );
+// studentList("Abdulloh","Umarov", "25", );
+
+// function calcAverage(a, b, c) {
+//   return((a + b + c) / 3);
+// }
+// console.log(calcAverage(15, 10, 12));
+
+// function calcAddation (a,b){
+//     return a+b
+// }
+
+
+// function calcAverage(a, b, c){
+//     return(a + b + c) / 3
+// }
+// console.log(calcAverage(10,20,calcAddation(2,2)));   
