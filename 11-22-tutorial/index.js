8de77535dbeecc4dc84c95690cc004fe32fee140
Shohid <shukurovshohid7@gmail.com>
2022-11-22 17:43:20 +0500

    Object

---
+++ 11-22-tutorial/index.js
@@ -0,0 +1,65 @@
+// let account = {
+//   name: "Shohid",
+//   surname: "shukurov",
+//   age: "20",
+//   "full name": "shohid shukur",
+// };
+//console.log(account.);
+
+// let key = "age"
+// console.log(account[key]);
+
+// Object.seal(account);
+// account.surname = "Abdulloh";
+
+// console.log(account.surname);
+// delete account.name;
+
+// console.log(account);
+// console.log("names" in account);
+
+// let name = "shohid";
+// let surname = "shukurov";
+
+// let acc = {
+//   name,
+//   surname,
+// };
+
+// console.log(acc);
+
+// const acc1 = {
+//   name: "shohid",
+// };
+
+// const acc2 = {};
+//  Object.assign(acc2, acc1);
+
+// console.log(acc2);
+
+// const acc = {
+//   name: "shohid",
+//   surname: "shukurov",
+//   age: 20,
+// };
+// for (const i in acc){
+//     console.log(i);
+// }
+
+// const acc = {
+//   name: "shohid",
+//   surname: "shukurov",
+// };
+
+// const acc1 = structuredClone(acc);
+
+// console.log(acc1);
+
+// const ac1 = {
+//   name: "shohid",
+//   surname: "shukurov",
+// };
+
+// const ac2 = structuredClone(ac1);
+// ac1.name = "shajaj";
+// console.log(ac2);
